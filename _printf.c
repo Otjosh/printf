0bac0135c31e39378958cf66bf7536cfb65151bc
Otjosh <jotagetvo@gmail.com>
2023-10-21 14:50:00 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,38 +1,64 @@
 #include "main.h"
-#include <stdarg.h>
 
+/**
+ * _printf - Format and print data according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-    int printed_chars = 0;
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	int i, char_count = 0;
 
-    while (*format)
-    {
-        if (*format == '%' && *(format + 1))
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                printed_chars += write(1, &c, 1);
-            }
-            else if (*format == 's')
-            {
-                char *s = va_arg(args, char *);
-                printed_chars += write(1, s, _strlen(s));
-            }
-            else if (*format == '%')
-            {
-                printed_chars += write(1, "%", 1);
-            }
-        }
-        else
-        {
-            printed_chars += write(1, format, 1);
-        }
-        format++;
-    }
-    va_end(args);
-    return printed_chars;
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			char_count++;
+		}
+		else
+		{
+			i++; /* Move past '%' */
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+				write(1, &c, 1);
+				char_count++;
+			}
+			else if (format[i] == 's')
+			{
+				char *str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					str = "(null)";
+				}
+				while (*str)
+				{
+					write(1, str, 1);
+					str++;
+					char_count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				char_count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				char_count++;
+				write(1, &format[i], 1);
+				char_count++;
+			}
+		}
+	}
+
+	va_end(args);
+
+	return (char_count);
 }
--- main.h
+++ main.h
@@ -2,7 +2,11 @@
 #define MAIN_H
 
 #include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
+int _strlen(const char *s);
+int _itoa(int num, char *str);
+int _utoa_base(unsigned int num, char *str, int base);
 
 #endif /* MAIN_H */
