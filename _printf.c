55be0e3626db1c500d6fdcd396d1a5afbaa6ce88
Otjosh <jotagetvo@gmail.com>
2023-10-30 08:10:17 -0700

    update

--- _printf.c
+++ _printf.c
@@ -8,60 +8,70 @@
  *
  * Return: The number of characters printed.
  */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, char_count = 0;
+	va_list args;
+	int i, char_count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] != '%')
-        {
-            write(1, &format[i], 1);
-            char_count++;
-        }
-        else
-        {
-            i++; /* Move past '%' */
-            if (format[i] == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                char_count++;
-            }
-            else if (format[i] == 's')
-            {
-                char *str = va_arg(args, char *);
+	for (i = 0; format[i] != '\0'; i++)
+	{
 
-                if (str == NULL)
-                {
-                    str = "(null)";
-                }
-                while (*str)
-                {
-                    write(1, str, 1);
-                    str++;
-                    char_count++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                write(1, "%", 1);
-                char_count++;
-            }
-            else
-            {
-                write(1, "%", 1);
-                char_count++;
-                write(1, &format[i], 1);
-                char_count++;
-            }
-        }
-    }
+	if (format[i] != '%')
+	{
+		write(1, &format[i], 1);
+		char_count++;
+	}
 
-    va_end(args);
+	else
+	{
+		i++; /* Move past '%' */
 
-    return char_count;
+		if (format[i] == 'c')
+		{
+			char c = va_arg(args, int);
+
+			write(1, &c, 1);
+			char_count++;
+		}
+
+		else if (format[i] == 's')
+		{
+
+			char *str = va_arg(args, char *);
+
+			if (str == NULL)
+		{
+			str = "(null)";
+		}
+
+		while (*str)
+		{
+			write(1, str, 1);
+			str++;
+		char_count++;
+		}
+		}
+
+		else if (format[i] == '%')
+		{
+			write(1, "%", 1);
+			char_count++;
+		}
+
+		else
+		{
+			write(1, "%", 1);
+			char_count++;
+			write(1, &format[i], 1);
+			char_count++;
+		}
+	}
+	}
+
+	va_end(args);
+
+	return (char_count);
 }
